589b477d904f143b62d0a4f5b7121f22eb4ca697
Lissa2121 <114819609+Lissa2121@users.noreply.github.com>
2024-07-20 17:15:10 +0300

    hw12.3

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,23 @@
+const params = {
+    key: "45012251-1fd54445ad7ef6677a1cc96fc",
+    q: "",
+    imageType: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+};
+
+export function generateHttpsQuery(formValue) {
+    params.q = formValue;
+    const queryString = new URLSearchParams(params).toString();
+    return `https://pixabay.com/api/?${queryString}`;
+}
+
+export function fetchPictures(httpsQuery) {
+    return fetch(httpsQuery)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+}
